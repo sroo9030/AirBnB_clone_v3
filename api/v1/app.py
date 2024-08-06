a415add3f852240dfcd445b99647aecd9a64e7d2
lenovo <eisra3000ese@gmail.com>
2024-08-07 01:09:31 +0300

    12

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,14 @@ This module defines the Flask application.
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
+	
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """
