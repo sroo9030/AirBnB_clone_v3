4f419bfe9a785763ae00d04cb5e9ab3d0e018089
lenovo <eisra3000ese@gmail.com>
2024-08-07 00:26:25 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module sets up the Flask application and registers the blueprint.
+This module defines the Flask application.
 """
 
 from flask import Flask
@@ -11,14 +11,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown_appcontext(exception):
     """
-    Closes the storage on teardown.
+    Calls storage.close() at the end of the request.
     """
     storage.close()
-
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
