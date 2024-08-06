3ad10cd3c93baf3e1b65e2e1aeecc3fed4060883
lenovo <eisra3000ese@gmail.com>
2024-08-07 00:36:00 +0300

    5

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """
@@ -18,12 +19,14 @@ def teardown_appcontext(exception):
     """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """
     JSON 404 error response.
     """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
