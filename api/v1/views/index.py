1a972785e54f9e74d79ff6c6675352a14732b3ed
lenovo <eisra3000ese@gmail.com>
2024-08-06 22:05:26 +0300

    3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,19 @@ def status():
     Returns the status of the API.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Retrieves the number of each object by type.
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
